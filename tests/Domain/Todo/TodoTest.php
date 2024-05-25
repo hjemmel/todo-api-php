d5d905bbd9a2c1c8dba3ad1da581c20cd099a892
Hans Jakob Emmel <hans.emmel@gmail.com>
2024-05-25 10:40:01 +1000

    chore: PHPunit 10.5

--- tests/Domain/Todo/TodoTest.php
+++ tests/Domain/Todo/TodoTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Tests\Domain\Todo;
@@ -8,7 +9,7 @@ use Tests\TestCase;
 
 class TodoTest extends TestCase
 {
-    public function todoProvider()
+    public static function todoProvider()
     {
         return [
             ["C137", 'Rick Sanchez', true],
